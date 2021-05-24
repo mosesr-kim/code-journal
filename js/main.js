7aca8888aebf7593f612c86790f4d5918547842d
Moses Kim <mosesrandykim@gmail.com>
2021-05-24 16:53:44 -0700

    added an event listener for 'input' on the photoURL and updated the src attribute to the photo preview

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 /* global data */
 /* exported data */
+var $photoURL = document.querySelector('.photoURL');
+var $image = document.querySelector('.image');
+$photoURL.addEventListener('input', function (event) {
+  $image.setAttribute('src', $photoURL.value);
+});
