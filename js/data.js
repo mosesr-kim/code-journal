96237893d4d4d0981492df6d7072adc8a789df70
Moses Kim <mosesrandykim@gmail.com>
2021-05-24 17:09:28 -0700

    put the form's input values into a new object named inputs

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var inputs = {};
+var $entryForm = document.querySelector('.entry-form');
+var $title = document.querySelector('.title');
+var $photoURL = document.querySelector('.photoURL');
+var $notes = document.querySelector('.notes');
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  inputs.title = $title.value;
+  inputs.photoURL = $photoURL.value;
+  inputs.notes = $notes.value;
+});
