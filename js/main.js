476a92331ed4ad3d68d3180ab145bf6c9bc82d78
Moses Kim <mosesrandykim@gmail.com>
2021-05-25 15:17:44 -0700

    used a loop to create a DOM tree for each journal entry in the data model and appended it to the page when the 'DOMContentLoaded' event was fired

--- js/main.js
+++ js/main.js
@@ -21,3 +21,44 @@ $entryForm.addEventListener('submit', function (event) {
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
 });
+
+function entryDOMTree(object) {
+  var liOut = document.createElement('li');
+
+  var divRow = document.createElement('div');
+  divRow.setAttribute('class', 'row');
+  liOut.appendChild(divRow);
+
+  var divColImg = document.createElement('div');
+  divColImg.setAttribute('class', 'column-half');
+  divRow.appendChild(divColImg);
+
+  var divColText = document.createElement('div');
+  divColText.setAttribute('class', 'column-half padding-right-0');
+  divRow.appendChild(divColText);
+
+  var img = document.createElement('img');
+  img.setAttribute('src', object.photoURL);
+  img.setAttribute('class', 'image margin-bot-25');
+  divColImg.appendChild(img);
+
+  var header = document.createElement('h1');
+  header.setAttribute('class', 'font-family margin-none');
+  divColText.appendChild(header);
+
+  var paragraph = document.createElement('p');
+  divColText.appendChild(paragraph);
+
+  header.textContent = object.title;
+  paragraph.textContent = object.notes;
+
+  return liOut;
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  var $ul = document.querySelector('ul');
+  for (var i = 0; i < data.entries.length; i++) {
+    var newEntry = entryDOMTree(data.entries[i]);
+    $ul.insertBefore(newEntry, $ul.childNodes[0]);
+  }
+});
