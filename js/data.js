42903607d378ffc3eb0d0f613f4de2f0a2839848
Moses Kim <mosesrandykim@gmail.com>
2021-05-24 17:49:25 -0700

    moved my work from data.js into main.js

--- js/data.js
+++ js/data.js
@@ -6,21 +6,3 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
-
-var inputs = {};
-var $entryForm = document.querySelector('.entry-form');
-var $title = document.querySelector('.title');
-var $photoURL = document.querySelector('.photoURL');
-var $notes = document.querySelector('.notes');
-var $image = document.querySelector('.image');
-$entryForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  inputs.title = $title.value;
-  inputs.photoURL = $photoURL.value;
-  inputs.notes = $notes.value;
-  inputs.entryID = data.nextEntryId;
-  data.nextEntryId++;
-  data.entries.push(inputs);
-  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $entryForm.reset();
-});
