99a9221cd820cafe404294a91685fe89a6728bef
Moses Kim <mosesrandykim@gmail.com>
2021-05-24 17:35:15 -0700

    reset the image preview's 'src' attribute

--- js/data.js
+++ js/data.js
@@ -12,6 +12,7 @@ var $entryForm = document.querySelector('.entry-form');
 var $title = document.querySelector('.title');
 var $photoURL = document.querySelector('.photoURL');
 var $notes = document.querySelector('.notes');
+var $image = document.querySelector('.image');
 $entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
   inputs.title = $title.value;
@@ -20,4 +21,5 @@ $entryForm.addEventListener('submit', function (event) {
   inputs.entryID = data.nextEntryId;
   data.nextEntryId++;
   data.entries.push(inputs);
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
 });
