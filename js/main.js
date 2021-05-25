ed661d4161ffdc7870eabec852537e448b53d3ba
Moses Kim <mosesrandykim@gmail.com>
2021-05-25 11:55:18 -0700

    removed container-main and also placed variable inputs into the function for the submit event

--- js/main.js
+++ js/main.js
@@ -6,19 +6,18 @@ $photoURL.addEventListener('input', function (event) {
   $image.setAttribute('src', $photoURL.value);
 });
 
-var inputs = {};
 var $entryForm = document.querySelector('.entry-form');
 var $title = document.querySelector('.title');
 var $notes = document.querySelector('.notes');
 $entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
+  var inputs = {};
   inputs.title = $title.value;
   inputs.photoURL = $photoURL.value;
   inputs.notes = $notes.value;
   inputs.entryID = data.nextEntryId;
   data.nextEntryId++;
   data.entries.push(inputs);
-  inputs = {};
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
 });
